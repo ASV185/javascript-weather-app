7a45b66629c02ca43e575a433f88fb9daf6e7fb7
ASV185 <74805696+ASV185@users.noreply.github.com>
2023-09-29 16:36:27 -0600

    added notes in JavaScript

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ let months = [
 ];
 let month = months[now.getMonth()];
 
+//added date
+
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
@@ -32,41 +34,43 @@ function displayWeatherCondition(response) {
   document.querySelector("#wind").innerHTML = response.data.wind.speed;
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
-}
+} //function allows to get specific weather information in openweathermap api
 
 function searchCity(city) {
   let apiKey = "ba0e4799e1fd496cce3683dfddd87bcc";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
-}
+} //Using api key, openweathermap api, and axios to receive the weather of city
 
 function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
   searchCity(city);
-}
+} //function receives value of search input of the city
 
 function searchLocation(position) {
   let apiKey = "ba0e4799e1fd496cce3683dfddd87bcc";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
-}
+} //function searches location with position parameter and latitude and longitude coordinates
 
 function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
-}
-//Feature #1
+} //Current location using Geolocation api
+
+//date feature
 
 let dateElement = document.querySelector("#date-section");
 dateElement.innerHTML = `${day}, ${month} ${date} ${hours}:${minutes}`;
 
-//Feature #2
+//search weather
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
 searchCity("El Paso");
 
+// Current location feature
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
