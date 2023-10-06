bd871c43cc4d56cb69f3143ddb423e4688204638
ASV185 <74805696+ASV185@users.noreply.github.com>
2023-10-05 21:02:42 -0600

    made changes to hours and minutes in the date element

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ let now = new Date();
 
 let date = now.getDate();
 let hours = now.getHours();
+if (hours > 10) {
+  hours = `0${hours}`;
+}
+
 let minutes = now.getMinutes();
-let days = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
 let day = days[now.getDay()];
 
 let months = [
